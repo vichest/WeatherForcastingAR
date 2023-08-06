5ef6c42649d819238958a3912e5c23c1edb8460c
vichest <129871281+vichest@users.noreply.github.com>
2023-08-06 22:02:34 +0530

    Initial Commit

---
+++ WeatherAR/WeatherARApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherARApp.swift
+//  WeatherAR
+//
+//  Created by Sanjit Pandit on 06/08/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherARApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
